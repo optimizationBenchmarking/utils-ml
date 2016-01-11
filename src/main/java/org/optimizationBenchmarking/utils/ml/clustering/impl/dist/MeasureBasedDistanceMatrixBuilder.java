45d4bdda2db734ed8826ef209e58e96c4df199fd
Thomas Weise <tweise@ustc.edu.cn>
2016-01-11 15:27:06 +0800

    MeasureBasedDistanceMatrixBuilder adapted to new Builder Structure
    We now make use of the improved distance matrix builder structure from
    utils-math.

--- src/main/java/org/optimizationBenchmarking/utils/ml/clustering/impl/dist/MeasureBasedDistanceMatrixBuilder.java
+++ src/main/java/org/optimizationBenchmarking/utils/ml/clustering/impl/dist/MeasureBasedDistanceMatrixBuilder.java
@@ -1,14 +1,16 @@
 package org.optimizationBenchmarking.utils.ml.clustering.impl.dist;
 
 import org.optimizationBenchmarking.utils.math.matrix.IMatrix;
-import org.optimizationBenchmarking.utils.math.matrix.impl.DoubleDistanceMatrix1DBuilder;
+import org.optimizationBenchmarking.utils.math.matrix.impl.DistanceMatrixBuilderJob;
+import org.optimizationBenchmarking.utils.math.statistics.aggregate.IAggregate;
 import org.optimizationBenchmarking.utils.ml.clustering.spec.IDistanceMeasure;
+import org.optimizationBenchmarking.utils.reflection.EPrimitiveType;
 
 /**
  * A builder for symmetric distance matrices based on a distance measure.
  */
 public final class MeasureBasedDistanceMatrixBuilder
-    extends DoubleDistanceMatrix1DBuilder {
+    extends DistanceMatrixBuilderJob {
 
   /** the data matrix */
   private final IMatrix m_matrix;
@@ -33,13 +35,21 @@ public final class MeasureBasedDistanceMatrixBuilder
 
   /** {@inheritDoc} */
   @Override
+  protected final EPrimitiveType getDistanceType() {
+    return EPrimitiveType.DOUBLE;
+  }
+
+  /** {@inheritDoc} */
+  @Override
   protected final int getElementCount() {
     return this.m_matrix.m();
   }
 
   /** {@inheritDoc} */
   @Override
-  protected final double getDistance(final int i, final int j) {
-    return this.m_measure.compute(this.m_matrix, i, this.m_matrix, j);
+  protected final void setDistance(final int i, final int j,
+      final IAggregate appendTo) {
+    appendTo.append(
+        this.m_measure.compute(this.m_matrix, i, this.m_matrix, j));
   }
 }
